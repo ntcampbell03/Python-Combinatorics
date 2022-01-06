074df9a47312d803de8c70d3857d94fc96380f5f
ntcampbell03 <41170757+ntcampbell03@users.noreply.github.com>
2022-01-05 19:01:22 -0600

    Update numberofintegersolutions.py

--- numberofintegersolutions.py
+++ numberofintegersolutions.py
@@ -7,23 +7,23 @@ Parameters:
     target: the value to be added to
     (eg. a target of 15 would correspond to a + b + c = 15).
 '''
-
 number_of_solutions = 0
-def Combinations(range_list, target, cur = 0, iter_list = []):
-    if iter_list == []:
-        iter_list = [0] * len(range_list)
-    if cur == len(range_list) - 1:
-        for iter_list[cur] in range(range_list[cur][0], range_list[cur][1] + 1):
-            if sum(iter_list) == target:
-                print(iter_list)
-                global number_of_solutions
-                number_of_solutions += 1
-    else:
-        for iter_list[cur] in range(range_list[cur][0], range_list[cur][1] + 1):
-            Combinations(range_list = range_list, target = target, cur = cur + 1, iter_list = iter_list)
+class NumberOfSolutions:
+    def Combinations(range_list, target, cur = 0, iter = []):
+        if iter == []:
+            iter = [0] * len(range_list)
+        if cur == len(range_list) - 1:
+            for iter[cur] in range(range_list[cur][0], range_list[cur][1] + 1):
+                if sum(iter) == target:
+                    print(iter)
+                    global number_of_solutions
+                    number_of_solutions += 1
+        else:
+            for iter[cur] in range(range_list[cur][0], range_list[cur][1] + 1):
+                NumberOfSolutions.Combinations(range_list = range_list, target = target, cur = cur + 1, iter = iter)
 
-def Solutions(range_list, target):
-    Combinations(range_list, target)
-    global number_of_solutions
-    print(number_of_solutions)
-    number_of_solutions = 0
+    def Solutions(range_list, target):
+        NumberOfSolutions.Combinations(range_list, target)
+        global number_of_solutions
+        print(number_of_solutions)
+        number_of_solutions = 0
